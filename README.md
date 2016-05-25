64a449c62dbb7949ac65addf9f1daab0aabe228b
Romko_Explosion <mylogin222@gmail.com>
2016-05-25 22:30:17 +0300

    seminar5

---
+++ README.md
@@ -0,0 +1,6 @@
+Following the steps:
+
+1. First open reposotiry https://github.com/romanborysenkoimz11/lab.git.
+2.  You need the Branch: Seminar05
+3. Then you need folder: REST
+4. Then you need files: Main, FileInfo, WrappedList, Massege,  UserInfo 
---
+++ REST/FileInfo.java
@@ -0,0 +1,65 @@
+package com.REST;
+
+
+	import java.io.File;
+	import java.io.IOException;
+	import java.nio.file.Files;
+	import java.util.Base64;
+	import java.util.Base64.Decoder;
+	import java.util.Base64.Encoder;
+
+	import javax.xml.bind.annotation.XmlAccessType;
+	import javax.xml.bind.annotation.XmlAccessorType;
+	import javax.xml.bind.annotation.XmlRootElement;
+
+	@XmlRootElement
+	@XmlAccessorType(XmlAccessType.FIELD)
+	public class FileInfo 
+	{
+		private String sender;
+		private String filename;
+		private String content;
+
+		public FileInfo() {
+		}
+		
+		public FileInfo(String sender, File file) throws IOException{
+			if(!file.exists() || file.isDirectory())
+				throw new IOException("The specified file "+file.getCanonicalPath()+ " does not exist or is not a file!");
+			
+			this.sender = sender;
+			this.filename = file.getName();
+			Encoder encoder = Base64.getEncoder();
+			this.content = encoder.encodeToString(Files.readAllBytes(file.toPath()));
+		}
+
+		public String getSender() {
+			return sender;
+		}
+
+		public void setSender(String sender) {
+			this.sender = sender;
+		}
+
+		public String getFilename() {
+			return filename;
+		}
+
+		public void setFilename(String filename) {
+			this.filename = filename;
+		}
+
+		public String getContent() {
+			return content;
+		}
+
+		public void setContent(String content) {
+			this.content = content;
+		}
+		
+		public byte[] getContentBytes(){
+			Decoder decoder = Base64.getDecoder();
+			return decoder.decode(this.content);
+		}
+	}
+
